6d1d9a7ddb9beba4afc2ee12dd39f7d61d16fe26
tuanhatran <trantuanha@gmail.com>
2016-06-29 00:35:35 +0200

    Refactor with second test

--- src/main/java/PokerHands.java
+++ src/main/java/PokerHands.java
@@ -8,21 +8,44 @@ class PokerHands {
     }
 
     public Result whoWin(final Player player1, final Player player2) {
+        final Result result;
         final HandType firstPlayerHandType = cardUtilities.highestHandTypeFrom(player1.getHand());
         final HandType secondPlayerHandType = cardUtilities.highestHandTypeFrom(player2.getHand());
-        if (firstPlayerHandType.ordinal() > secondPlayerHandType.ordinal()) {
-            return new Result(player1, firstPlayerHandType, Optional.empty());
-        } else if (secondPlayerHandType.ordinal() > firstPlayerHandType.ordinal()) {
-            return new Result(player2, secondPlayerHandType, Optional.empty());
+        if (winableByHandType(firstPlayerHandType, secondPlayerHandType)) {
+            result = whoWinByHandType(player1, firstPlayerHandType, player2, secondPlayerHandType);
         } else {
-            final Card firstPlayerHighestCard = cardUtilities.highestCardFrom(player1.getHand());
-            final Card secondPlayerHighestCard = cardUtilities.highestCardFrom(player2.getHand());
+            result = whoWinByHigherCard(player1, player2);
 
-            if (firstPlayerHighestCard.getRank().ordinal() > secondPlayerHighestCard.getRank().ordinal()) {
-                return new Result(player1, HandType.HIGH_CARD, Optional.of(firstPlayerHighestCard));
-            } else {
-                return new Result(player2, HandType.HIGH_CARD, Optional.of(secondPlayerHighestCard));
-            }
         }
+        return result;
+    }
+
+    private Result whoWinByHandType(final Player player1,
+                                    final HandType firstPlayerHandType,
+                                    final Player player2,
+                                    final HandType secondPlayerHandType) {
+        final Result result;
+        if (firstPlayerHandType.ordinal() > secondPlayerHandType.ordinal()) {
+            result = new Result(player1, firstPlayerHandType, Optional.empty());
+        } else {
+            result = new Result(player2, secondPlayerHandType, Optional.empty());
+        }
+        return result;
+    }
+
+    private Result whoWinByHigherCard(final Player player1, final Player player2) {
+        final Result result;
+        final Card firstPlayerHighestCard = cardUtilities.highestCardFrom(player1.getHand());
+        final Card secondPlayerHighestCard = cardUtilities.highestCardFrom(player2.getHand());
+        if (firstPlayerHighestCard.getRank().ordinal() > secondPlayerHighestCard.getRank().ordinal()) {
+            result = new Result(player1, HandType.HIGH_CARD, Optional.of(firstPlayerHighestCard));
+        } else {
+            result = new Result(player2, HandType.HIGH_CARD, Optional.of(secondPlayerHighestCard));
+        }
+        return result;
+    }
+
+    private boolean winableByHandType(final HandType firstHandType, final HandType secondHandType) {
+        return firstHandType != secondHandType;
     }
 }
