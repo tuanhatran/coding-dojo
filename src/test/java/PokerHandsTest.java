3af7f9b4f67eae5dc7512964c7af65e08f43e4f3
Tuan Ha TRAN <trantuanha@gmail.com>
2016-06-28 23:01:59 +0200

    itinial commit

---
+++ src/test/java/PokerHandsTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by thtran on 28/06/16.
+ */
+public class PokerHandsTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+}
